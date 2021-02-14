9746ec207c683658d4e1e9b2955985a7850bd961
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-02-14 21:05:15 +0000

    Handling form submission
    - Handling data from form
    - removing sample line from list upon form submission
    - refinment on html structure
    - append bucketList item to list

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,60 @@
-// TODO remove when development complete
-console.log('Jquery script loaded')
-
-// Section for Jumbotron Bucket list hide and toggle Event listener
+/**
+ *  Jumbotron
+ */
 $('#collapse-bucketList-info').hide();
-
-$('#bucketlist-description').click(function (e) { 
-    e.preventDefault();
-    $('#collapse-bucketList-info').toggle('slow');
+$('#bucketlist-description').click(function(e) {
+  e.preventDefault();
+  $('#collapse-bucketList-info').toggle('slow');
 });
+
+/**
+ * Form submit
+ */
+
+$('#add-bucketlist-item-form').submit(function(e) {
+  e.preventDefault();
+  const bucketListItem = {
+    'action': $("#bucketlist-item").val(),
+    'location': $("#location").val() || null,
+    'datePlanned': $("#date-planned").val() || null
+  }
+  if (bucketListItem['action']) {
+    AddItemToBucketList(bucketListItem)
+  }
+});
+
+/**
+ * Append your goal to your Bucket List
+ * @param {*} bucketListItem
+ */
+function AddItemToBucketList(bucketListItem) {
+  let newItem = `<li>${bucketListItem['action']}`
+  if (bucketListItem['location']) {
+    newItem += `, ${bucketListItem['location']}`
+  }
+  if (bucketListItem['datePlanned']) {
+    // TODO make date human readable
+    newItem += `, ${bucketListItem['datePlanned']}`
+  }
+  newItem += `.</li>`
+  /* remove first sample Item */
+  removeEmptyLines()
+  /* Add Item to list */
+  $('#bucketList').prepend(newItem);
+}
+
+/**
+ * Clean-up Sample entry from list 
+ */
+function removeEmptyLines() {
+  /** removes sample line on first prepend */
+  const lastLine = $('#bucketList').children('li').last();
+  if (!lastLine.text()) {
+    lastLine.remove();
+  }
+
+  let firstItem = $('.removed-on-first-iteraction').text()
+  if (firstItem === 'Add your first Item') {
+    $('.removed-on-first-iteraction').remove();
+  }
+}
